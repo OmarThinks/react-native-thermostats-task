15910f21c722c28455f4fe060d4c70d7b77bc671
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2026-02-03 15:02:19 +0200

    feat: added Redux

--- __docs/TODO.md
+++ __docs/TODO.md
@@ -2,7 +2,7 @@
 
 - Reset Expo project ✅
 - src folder ✅
-- Add Native Wind
+- Add Native Wind ✅
 - Add Redux
 - Implement the UI
 - Think about the plan
--- expo-app/src/app/_layout.tsx
+++ expo-app/src/app/_layout.tsx
@@ -1,6 +1,12 @@
 import { Stack } from "expo-router";
 import "@/global.css";
+import { Provider } from "react-redux";
+import { store } from "@/redux/store";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Provider store={store}>
+      <Stack />
+    </Provider>
+  );
 }
